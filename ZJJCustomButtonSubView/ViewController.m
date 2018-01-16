3f834c1ae7a3ef6a5ec0a1264c56a7a5b37f74e9
张锦江 <33757152@qq.com>
2018-01-16 14:24:22 +0800

    初次提交

--- ZJJCustomButtonSubView/ViewController.m
+++ ZJJCustomButtonSubView/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ZJJButton.h"
 
 @interface ViewController ()
 
@@ -17,8 +18,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ZJJButton *button = [[ZJJButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100) withType:0];
+    [button setImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
+    [button setTitle:@"come go" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(comeOut) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+    [self creatView];
 }
 
+- (void)creatView {
+    ZJJButton *button = [[ZJJButton alloc] initWithFrame:CGRectMake(100, 300, 100, 40) withType:1];
+    [button setImage:[UIImage imageNamed:@"1"] forState:UIControlStateNormal];
+    [button setTitle:@"buybuy.." forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(comeOutRight) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void)comeOut {
+    NSLog(@"uuuuuuu......");
+}
+
+- (void)comeOutRight {
+    NSLog(@"rrrrrr......");
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
